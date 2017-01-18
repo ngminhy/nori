740bde77e727ba7d186aaddf4cc34fce02b0530d
xuanlv0703 <levanxuan@gmail.com>
2017-01-18 10:28:10 +0700

    error page

---
+++ Backend/Biz4CMS/Controllers/ErrorController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Biz4CMS.Controllers
+{
+    public class ErrorController : Controller
+    {
+        //
+        // GET: /Error/
+
+        public ActionResult Index()
+        {
+            return View("Error");
+        }
+
+        public ActionResult NotFound()
+        {
+            Response.StatusCode = 200;
+            return View("NotFound");
+        }
+
+        public ActionResult InternalServer()
+        {
+            Response.StatusCode = 200;
+            return View("InternalServer");
+        }
+
+    }
+}
