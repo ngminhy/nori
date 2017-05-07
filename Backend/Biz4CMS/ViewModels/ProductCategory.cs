809a7dbecf35483a0751c7993fbc093b3ff59d52
MinhY.Nguyen <ngminhy@gmail.com>
2017-05-07 12:56:31 +0700

    update blog

---
+++ Backend/Biz4CMS/ViewModels/ProductCategory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Biz4CMS.Models;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace Biz4CMS.ViewModels
+{
+    public class ProductCategory
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public string CategoryPageURL { get; set; }
+        public List<BriefProductDto> Products { get; set; }
+        public int NumofItem { get; set; }
+
+
+    }
+}
