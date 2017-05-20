a05ebcdf59ecab0d6821862372e248d11794c1b4
HieuHuynh <HieuHuynh@DESKTOP-8R43DHM>
2017-05-20 22:33:50 +0700

    shopping cart

--- Backend/Biz4CMS/Content/Assets/js/cart.js
+++ Backend/Biz4CMS/Content/Assets/js/cart.js
@@ -1,48 +1,40 @@
 jQuery(document).ready(function($){
-
 	var cartWrapper = $('.cd-cart-container');
-	var productId = 0;
-	if( cartWrapper.length > 0 ) {
-		var cartBody = cartWrapper.find('.body')
-		var cartList = cartBody.find('ul').eq(0);
-		var cartTotal = cartWrapper.find('.checkout').find('span');
-		var cartTrigger = cartWrapper.children('.cd-cart-trigger');
-		var cartCount = cartTrigger.children('.count')
-		var addToCartBtn = $('.btn-addcart');
-
-		//add product to cart
-		addToCartBtn.on('click', function(event){
-			event.preventDefault();
-			addToCart($(this));
-		});
-
-		//open/close cart
-		cartTrigger.on('click', function(event){
-			event.preventDefault();
-			toggleCart();
-		});
-
-		//close cart when clicking on the .cd-cart-container::before (bg layer)
-		cartWrapper.on('click', function(event){
-			if( $(event.target).is($(this)) ) toggleCart(true);
-		});
-	}
-
+	var cartBody = cartWrapper.find('.body')
+	var cartList = cartBody.find('ul').eq(0);
+	var cartTotal = cartWrapper.find('.checkout').find('span');
+	var cartTrigger = cartWrapper.children('.cd-cart-trigger');
+	var cartCount = cartTrigger.children('.count')
+	var addToCartBtn = $('.btn-addcart');
+	addToCartBtn.on('click', function(event){
+		event.preventDefault();
+		addToCart($(this));
+	});
+	cartTrigger.on('click', function(event){
+		event.preventDefault();
+		toggleCart();
+	});
+	cartWrapper.on('click', function(event){
+		if( $(event.target).is($(this)) ) toggleCart(true);
+	});
+	if(localStorage.length == 0) {
+		cartWrapper.addClass('empty')
+	} else {
+		cartWrapper.removeClass('empty');
+		initProduct();
+	};
 	function toggleCart(bool) {
 		var cartIsOpen = ( typeof bool === 'undefined' ) ? cartWrapper.hasClass('cart-open') : bool;
-		
-		if( cartIsOpen ) {
+		if( cartIsOpen) {
 			cartWrapper.removeClass('cart-open');
 			setTimeout(function(){
 				cartBody.scrollTop(0);
-				//check if cart empty to hide it
-				if( Number(cartCount.find('li').eq(0).text()) == 0) cartWrapper.addClass('empty');
+				if(localStorage.length == 0) cartWrapper.addClass('empty');
 			}, 500);
 		} else {
 			cartWrapper.addClass('cart-open');
 		}
 	}
-
 	function addToCart(trigger) {
 		var prouctItem = {
 		    id: trigger.attr('data-product-id'),
@@ -59,84 +51,29 @@ jQuery(document).ready(function($){
 		count ++;
 		prouctItem.count = count;
 		localStorage.setItem(prouctItem.id, JSON.stringify(prouctItem));
-		var cartIsEmpty = cartWrapper.hasClass('empty');
-		// //update cart product list
-		addProduct(prouctItem);
-
-		// //update number of items 
-		// updateCartCount(cartIsEmpty);
-		// // //update total price
-		// updateCartTotal(trigger.data('price'), true);
-		// // //show cart
-		cartWrapper.removeClass('empty');
+		initProduct();
 	}
-
-	function addProduct(prouctItem) {
-		var name = prouctItem.name;
-		var img = prouctItem.img.replace('~', '');
-		var price = numeral(prouctItem.price).format('0,0');
-		var count = prouctItem.count;
-
-		console.log(prouctItem);
-
-		var productAdded = $('<li class="product">'+
-			'<div class="product-image">'+
-			'<a href="#"><img src="'+img+'"></a></div>'+
-			'<div class="product-details">'+
-			'<h5><a href="">'+name+'</a></h5>'+
-			'<div class="actions"><span class="qty">'+count+'</span> x <span class="price">'+price+' VNĐ</span></div>'+
-			'</div></li>');
-		cartList.prepend(productAdded);
+	function initProduct() {
+		cartList.html("");
+		Object.keys(localStorage).forEach(function (key) {
+			var itemlocal = JSON.parse(localStorage.getItem(key));
+			var name = itemlocal.name;
+			var img = itemlocal.img.replace('~', '');
+			var price = numeral(itemlocal.price).format('0,0');
+			var count = itemlocal.count;
+			var productAdded = $('<li class="product">'+
+				'<div class="product-image">'+
+				'<a href="#"><img src="'+img+'"></a></div>'+
+				'<div class="product-details">'+
+				'<h5><a href="">'+name+'</a></h5>'+
+				'<div class="actions"><span class="qty">'+count+'</span> x <span class="price">'+price+' VNĐ</span></div>'+
+				'</div></li>');
+			cartList.prepend(productAdded);
+		})
+		if(localStorage.length != 0) cartWrapper.removeClass('empty');
+		updateCartCount();
 	}
-
-	// function quickUpdateCart() {
-	// 	var quantity = 0;
-	// 	var price = 0;
-		
-	// 	cartList.children('li:not(.deleted)').each(function(){
-	// 		var singleQuantity = Number($(this).find('select').val());
-	// 		quantity = quantity + singleQuantity;
-	// 		price = price + singleQuantity*Number($(this).find('.price').text().replace('$', ''));
-	// 	});
-
-	// 	cartTotal.text(price.toFixed(2));
-	// 	cartCount.find('li').eq(0).text(quantity);
-	// 	cartCount.find('li').eq(1).text(quantity+1);
-	// }
-
-	function updateCartCount(emptyCart, quantity) {
-		if( typeof quantity === 'undefined' ) {
-			var actual = Number(cartCount.find('li').eq(0).text()) + 1;
-			var next = actual + 1;
-			
-			if( emptyCart ) {
-				cartCount.find('li').eq(0).text(actual);
-				cartCount.find('li').eq(1).text(next);
-			} else {
-				cartCount.addClass('update-count');
-
-				setTimeout(function() {
-					cartCount.find('li').eq(0).text(actual);
-				}, 150);
-
-				setTimeout(function() {
-					cartCount.removeClass('update-count');
-				}, 200);
-
-				setTimeout(function() {
-					cartCount.find('li').eq(1).text(next);
-				}, 230);
-			}
-		} else {
-			var actual = Number(cartCount.find('li').eq(0).text()) + quantity;
-			var next = actual + 1;
-			
-			cartCount.find('li').eq(0).text(actual);
-			cartCount.find('li').eq(1).text(next);
-		}
-	}
-
-	function updateCartTotal(price, bool) {
-		bool ? cartTotal.text( (Number(cartTotal.text()) + Number(price)).toFixed(2) )  : cartTotal.text( (Number(cartTotal.text()) - Number(price)).toFixed(2) );
+	function updateCartCount() {
+		if(localStorage.length != 0) cartCount.find('li').text(localStorage.length);
 	}
 });
