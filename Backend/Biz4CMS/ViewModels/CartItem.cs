ea87bbf69506292efe99cbad12ab3faf0e3cc53e
xuanlv0703 <levanxuan@gmail.com>
2017-05-23 00:36:10 +0700

    shopping cart check

---
+++ Backend/Biz4CMS/ViewModels/CartItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Biz4CMS.ViewModels
+{
+   
+    public class CartItem
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int price { get; set; }
+        public string img { get; set; }
+        public int count { get; set; }
+    }
+}
